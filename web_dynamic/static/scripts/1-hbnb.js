270acf9c13da3497de98f44bcd4d04e484bd0a19
alaedine07 <alaedineboufaden@gmail.com>
2021-09-25 18:30:40 +0100

    amenities selection

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,3 +1,20 @@
-$( document ).ready(
-	
-);
+// $('document').ready(function(){
+//     // console.log('Hello world');
+//     $('INPUT[type="checkbox"]').on('change', function(){
+//         // console.log('state changed');
+//         //amenity_name = $(this).attr('data-name');
+//         console.log('hello world');
+//     });
+// });
+
+$('document').ready(function () {
+    let amenities = {};
+    $('INPUT[type="checkbox"]').change(function () {
+      if ($(this).is(':checked')) {
+        amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+      } else {
+        delete amenities[$(this).attr('data-id')];
+      }
+      $('.amenities H4').text(Object.values(amenities).join(', '));
+    });
+  });
