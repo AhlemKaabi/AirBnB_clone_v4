2595c99cfda99a31c2c7c5f3024ae35cd5e69523
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-09-26 18:51:03 +0100

    task 2 is done

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,17 +1,22 @@
 $('document').ready(
 	function () {
+    let keep_amenities = {}
+
 		$('input[type="checkbox"]').click(
 			function () {
-				if($(this).is(":checked")){
 
+				if($(this).is(":checked")){
+          // console.log(amenities[$(this)])
 					console.log("Checkbox is checked.");
-					console.log(this.getAttribute("data-id"));
+					keep_amenities[$(this).attr("data-id")] =  $(this).attr("data-name")
 				}
-				else if($(this).is(":not(:checked)")){
+				else if ($(this).is(":not(:checked)")){
 					console.log("Checkbox is unchecked.");
+          delete keep_amenities[$(this).attr("data-id")]
 				}
+        console.log(Object.values(keep_amenities));
+        $('.amenities h4' ).text(Object.values(keep_amenities))
 			}
 		);
 	}
 );
-
