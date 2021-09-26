f62c157839c1b2e9299a09ccfcd6079b337b501c
alaedine07 <alaedineboufaden@gmail.com>
2021-09-26 01:40:20 +0100

    fetch places

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
-""" Index """
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
-from models import storage
+"""
+Flask route that returns json status response
+"""
 from api.v1.views import app_views
-from flask import jsonify
+from flask import request, jsonify
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status', strict_slashes=False)
 def status():
-    """ Status of API """
-    return jsonify({"status": "OK"})
+    """ return JSON response OK """
+    if request.method == 'GET':
+        return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def number_objects():
-    """ Retrieves the number of each objects by type """
-    classes = [Amenity, City, Place, Review, State, User]
-    names = ["amenities", "cities", "places", "reviews", "states", "users"]
-
-    num_objs = {}
-    for i in range(len(classes)):
-        num_objs[names[i]] = storage.count(classes[i])
-
-    return jsonify(num_objs)
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """ retrives the number of each objects by type """
+    from models import storage
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+    objects_stats = {}
+    objects_stats["amenities"] = storage.count(Amenity)
+    objects_stats["cities"] = storage.count(City)
+    objects_stats["places"] = storage.count(Place)
+    objects_stats["reviews"] = storage.count(Review)
+    objects_stats["states"] = storage.count(State)
+    objects_stats["users"] = storage.count(User)
+    return jsonify(objects_stats)
--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -5,6 +5,20 @@ $.ajax({
     data: '{}',
     contentType: 'application/json',
     dataType: 'json',
-    success: function(data){console.log(data);}
+    success: function(data){
+        console.log(data);
+        for(let i = 0; i < data.length; i++)
+        {
+        $('section.places').append('<article>' + '<div class="title_box"><h2>' + data[i].name
+        + '</h2>\n<div class="price_by_night">$' + data[i].price_by_night +
+        '</div>\n</div>\n<div class="information"><div class="max_guest">' + data[i].max_guest +
+        ' guests </div>\n<div class="number_rooms">' + data[i].number_rooms +
+        ' Bedrooms</div><div class="number_bathrooms">' + data[i].number_bathrooms +
+        ' Bathrooms</div>' + '</div>' +
+        '<div class="user"><b>Owner: </b>' + data[i].first_name  + //data[i].place.user.last_name//
+        '</div>' + '<div class="description">' + data[i].description + '</div>'
+        );
+        }
+    }
 });
 });
