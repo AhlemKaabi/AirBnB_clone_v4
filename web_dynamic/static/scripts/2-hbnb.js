ee3cb24716257dcc1a81df53c668fd339742f7c9
alaedine07 <alaedineboufaden@gmail.com>
2021-09-26 23:15:55 +0100

    add all

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,11 +1,10 @@
-$('document').ready(function(){
-    const api_URL = 'http://' + window.location.hostname + ':5001/api/v1/status';
-    $.get(api_URL, function(data){
-        if (data.status === "OK"){
-            $('#api_status').addClass('available');
-        }
-        else{
-            $('#api_status').removeClass('available');
-        }
-    });
+$('document').ready(function () {
+  const ApiUrl = 'http://' + window.location.hostname + ':5001/api/v1/status';
+  $.get(ApiUrl, function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
 });
